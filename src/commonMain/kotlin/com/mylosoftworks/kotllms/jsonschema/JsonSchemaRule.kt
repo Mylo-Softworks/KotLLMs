346ad46dbb6d8fea5a54b18213a8850dbf5b332d
Mylo <36931363+gitmylo@users.noreply.github.com>
2025-02-12 13:59:18 +0100

    Fix JVM platform declaration clash

--- src/commonMain/kotlin/com/mylosoftworks/kotllms/jsonschema/JsonSchemaRule.kt
+++ src/commonMain/kotlin/com/mylosoftworks/kotllms/jsonschema/JsonSchemaRule.kt
@@ -3,6 +3,7 @@ package com.mylosoftworks.kotllms.jsonschema
 import com.mylosoftworks.gbnfkotlin.GBNF
 import com.mylosoftworks.kotllms.features.ToJson
 import kotlinx.serialization.json.JsonElement
+import kotlin.jvm.JvmName
 
 /**
  * Represents a single rule from a json schema, which can be built to a Json Element.
@@ -12,6 +13,7 @@ abstract class JsonSchemaRule: ToJson {
 
     override fun toJson(): JsonElement = build()
 
+    @JvmName("buildGBNFAlt")
     fun buildGBNF(gbnf: GBNF) = gbnf.buildGBNF() // For outside access
     abstract fun GBNF.buildGBNF() // For easy implementation (automatic this receiver)
 }
